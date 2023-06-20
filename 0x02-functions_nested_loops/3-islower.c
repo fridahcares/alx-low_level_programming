9257d68e2b07246579e7ed7ddf93d8b4f5a66811
fridahcares <fridahpaul02@gmail.com>
2023-06-20 12:11:05 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - lowercase character
+ * @c: The character in ASCII code
+ * Return: 1 if lowercase and 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+
+}
