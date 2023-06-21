1dd5646d23c9ccd36ad2ffbb09784ff1d16ff5fb
fridahcares <fridahpaul02@gmail.com>
2023-06-21 01:55:01 -0700

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add_integers - adds two integers and returns the result
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,2 +1,10 @@
 #include "main.h"
 
+/**
+ * times_table -  prints the 9 times table
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+
