2f421bc433154fcf8942e9e7be0b529e703778c6
fridahcares <fridahpaul02@gmail.com>
2023-06-21 06:42:33 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,13 @@ int main(void)
 {
 	int i, sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	while (i < 1024)
 	{
 		if ((i / 3) == 0 || (i / 5) == 0)
+		{
 			sum += i;
+		}
+		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
