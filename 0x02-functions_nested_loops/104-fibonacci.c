f3e9d6f37bbcd3e06009508ff9082afd8cf08b3d
fridahcares <fridahpaul02@gmail.com>
2023-06-21 12:44:54 -0700

    first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints the first Fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int a;
+	unsigned long int b;
+	unsigned long int c;
+
+	a = 1;
+	b = 2;
+
+	for (i = 1; i < 98; i++)
+	{
+		printf("%ld, ", a);
+		c = a + b;
+		a = b;
+		b = c;
+		if (i != 98)
+		{
+			printf("%ld", c);
+		}
+	}
+	printf("\n");
+	return (0);
+}
