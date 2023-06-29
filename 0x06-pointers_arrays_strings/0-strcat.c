211189ea47bc1118ad25d1a9848ab49bec4d8ee9
fridahcares <fridahpaul02@gmail.com>
2023-06-29 13:30:00 -0700

    hello world

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char* ptr = dest;
+	while(*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	ptr = '\0';
+	return (dest);
+}
