0e8b177d1861130db1c6cee77f33d749dc1ae573
fridahcares <fridahpaul02@gmail.com>
2023-07-03 15:42:57 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string to be checjed
+ * @needle: substring
+ * Return: pointer or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
