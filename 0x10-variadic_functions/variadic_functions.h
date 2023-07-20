1a2d8c335e5f76998a627e8b9da6f0ccb429b3cf
fridahcares <fridahpaul02@gmail.com>
2023-07-20 09:44:18 -0700

    sum them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_H
-#define _VARIADIC__H
+#ifndef _VARIADIC_H_
+#define _VARIADIC__H_
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
