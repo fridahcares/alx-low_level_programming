f8a0372e8ef4dd1ce26ddc82eb39e6c3861804cc
fridahcares <fridahpaul02@gmail.com>
2023-07-31 13:31:06 -0700

    delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int j;
+	unsigned int j = 0;
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 
@@ -31,6 +31,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	current = temp->next;
+	if (!current)
+		return (-1);
+
 	temp->next = current->next;
 	free(current);
 	return (1);
