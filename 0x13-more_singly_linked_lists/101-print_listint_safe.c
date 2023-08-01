81855d91b42fe8d854b42b5c07f4411402d8a8dc
fridahcares <fridahpaul02@gmail.com>
2023-08-01 03:33:34 -0700

    print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,48 @@
 #include "lists.h"
+#include <stdio.h>
+size_t loped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+/**
+ * looped_listint_len - counts number of nodes in linked list
+ * @head: pointer to the head of listint_t
+ * Return: 0 if the list is not looped
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t count = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				count++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				count++;
+				tortoise = tortoise->next;
+			}
+			return (count);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
 /**
  * print_listint_safe - function that prints a listint_t linked list
  * @head: a pointer to the head of the listint_t
@@ -6,26 +50,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t count = 0;
+	size_t count, index = 0;
 
-	tortoise = head;
-	hare = head;
+	count = looped_listint_len(head);
 
-	while (tortoise && hare && hare->next)
+	if (count == 0)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
+		for (; head != NULL; count++)
 		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			printf("-> [%p] %d\n", (void *)hare, hare->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		count++;
+	}
+	else
+	{
+		for (index = 0; index < count; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (count);
 }
