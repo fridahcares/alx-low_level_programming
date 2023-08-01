b250869227e933264c546d19e024d31c21eb808a
fridahcares <fridahpaul02@gmail.com>
2023-08-01 10:52:34 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-void print_message(void) __attribute__((constructor));
+
+void first(void) __attribute__((constructor));
+
 /**
- * print_message - function that prints the required message
+ * first - function that prints the required message
  */
-void print_message(void)
+void first(void)
 {
 	printf("You're bea;t! an;d yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
