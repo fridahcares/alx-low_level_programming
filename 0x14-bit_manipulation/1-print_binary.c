aa781030f3d2b4ad982283d294759dbcc1d56c5d
fridahcares <fridahpaul02@gmail.com>
2023-08-03 07:27:45 -0700

    get bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_binary - prints the binary representation of a number
  * @n: the integer to be converted
@@ -13,6 +13,7 @@ void print_binary(unsigned long int n)
 	for (j = 63; j >= 0; j++)
 	{
 		current = n >> j;
+
 		if (current & 1)
 		{
 			_putchar('1');
@@ -22,5 +23,5 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 	}
 	if (!count)
-		_putchar ('0');
+		_putchar('0');
 }
