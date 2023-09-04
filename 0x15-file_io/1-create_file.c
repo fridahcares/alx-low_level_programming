1d8a05727330fed2037d4d1974174ba772f1c624
fridahcares <fridahpaul02@gmail.com>
2023-09-04 12:42:49 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
+
+	if (text_content != NULL)
 	{
 		for (strlen = 0; text_content[strlen];)
 			strlen++;
